562201c4e1ab0cd8c3e9191db04c222f360bb66c
Benjamin Lorentz <bjl34716@uga.edu>
2023-03-27 14:50:14 -0400

    update contam script
    - properly load the phyloseq obj now

--- r_scripts/contam_script.r
+++ r_scripts/contam_script.r
@@ -6,13 +6,29 @@ library(phyloseq)
 library(decontam)
 
 #load data into memory
-table_dada2 <- "results/qiime2/input/table.qza"
+table_dada2 <- "results/dada2/ASV_table.tsv"
 rooted_tree <- "results/qiime2/phylogenetic_tree/rooted-tree.qza"
 taxonomy_file <- "results/qiime2/input/taxonomy.qza"
 metadata_file <- "metadata.tsv"
 
 #create phyloseq Obj
-table_phylo <- qza_to_phyloseq(table_dada2,rooted_tree,taxonomy_file,metadata_file)
+metadata<- read_q2metadata(metadata_file)
+metadata <- metadata %>% remove_rownames %>% column_to_rownames(var="SampleID")
+metadata_ps <- sample_data(metadata)
+
+table <- data.frame(read_table(table_dada2))
+rownames(table) <- table$ASV_ID
+table <- table %>% select(-c("ASV_ID"))
+table_ps <- otu_table(table, taxa_are_rows=T)
+
+tax_obj <- read_qza(taxonomy_file)
+nice_tax <- tax_table(matrix(parse_taxonomy(tax_obj$data)))
+tax_ps <- tax_table(as.matrix(nice_tax))
+
+tree_obj <- read_qza(rooted_tree)
+tree <- tree_obj$data
+
+ps <- phyloseq(table_ps, tax_ps, tree, metadata_ps)
 
 #TODO filter out Negative control samples
 
