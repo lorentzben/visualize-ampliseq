e3c9cbcdc0798a360b620ee5310af283c33221d5
Benjamin Lorentz <bjl34716@uga.edu>
2023-03-28 14:51:41 -0400

    update contam_script.r
    - included code to identify and filter contamination based on negative control samples

--- r_scripts/contam_script.r
+++ r_scripts/contam_script.r
@@ -12,31 +12,42 @@ rooted_tree <- "results/qiime2/phylogenetic_tree/rooted-tree.qza"
 taxonomy_file <- "results/qiime2/input/taxonomy.qza"
 metadata_file <- "metadata.tsv"
 
-#create phyloseq Obj
+#format metadata in phyloseq compatable
 metadata<- read_q2metadata(metadata_file)
 metadata <- metadata %>% remove_rownames %>% column_to_rownames(var="SampleID")
 metadata_ps <- sample_data(metadata)
 
+# format ASV table into phyloseq compatable
 table <- data.frame(read_table(table_dada2))
 rownames(table) <- table$ASV_ID
 table <- table %>% select(-c("ASV_ID"))
 table_ps <- otu_table(table, taxa_are_rows=T)
 
+# format the taxonomy data into phylsoeq compatable
 tax_obj <- read_qza(taxonomy_file)
 tax_ps <- tax_table(as.matrix(parse_taxonomy(tax_obj$data)))
 
-
+# format the tree into phyloseq
 tree_obj <- read_qza(rooted_tree)
 tree <- tree_obj$data
 
+# generate the phyloseq object
 ps <- phyloseq(table_ps, tax_ps, tree, metadata_ps)
 
+# add a variable for negative control samples, update list if needed
+sample_data(ps)$is.neg <- sample_data(ps)$Treatment %in% c("NC","Control","Negative Control","control","negative control")
 
+# identify the possible contaminats based on 0.5 
+# In the prevalence test there is a special value worth knowing, threshold=0.5, 
+#that will identify as contaminants all sequences thare are more prevalent in negative controls than in positive samples.
 
-#TODO filter out Negative control samples
+contamdf.prev05 <- isContaminant(ps, method="prevalence", neg="is.neg", threshold=0.5)
 
-#This is biom v1.0 can we turn it into a hd5 format?
-#biom <- make_biom(data=otu_table(ps), sample_metadata=sample_data(ps), observation_metadata=tax_table(ps))
-biom <- make_biom(data=otu_table(ps))
+# filter the phyloseq object to remove contamination
+ps.noncontam <- phyloseq::prune_taxa(!contamdf.prev05$contaminant,ps)
 
+# turn phyloseq object into a biom file
+biom <- make_biom(data=otu_table(ps.noncontam))
+
+# save biom file to disk
 write_biom(biom, "dada2-filtered-table.biom")
