c1727215db988b197d906a8b6946a649220e7b0a
Benjamin Lorentz <bjl34716@uga.edu>
2023-04-20 13:41:46 -0400

    update render_srs and srs_curve rmd
    - add print in both cases
    - fix the cmax/cmax_file names

--- bin/render_srs.sh
+++ bin/render_srs.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+ls
+
 Rscript -e "rmarkdown::render('srs_curve.rmd', output_file='$PWD/srs_curve.pdf', output_format='pdf_document', clean=TRUE, knit_root_dir='$PWD')"
 Rscript -e "rmarkdown::render('srs_curve.rmd', output_file='$PWD/srs_curve.html', output_format='html_document', clean=TRUE, knit_root_dir='$PWD')"
