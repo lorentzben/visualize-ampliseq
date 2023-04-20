4f8498ad73f8b8f77f8e643e9b639ea91e71609f
Benjamin Lorentz <bjl34716@uga.edu>
2023-04-20 14:01:48 -0400

    fix the srs cmax command
    - echo pwd for render command

--- bin/render_srs.sh
+++ bin/render_srs.sh
@@ -2,5 +2,7 @@
 
 ls
 
+echo $PWD
+
 Rscript -e "rmarkdown::render('srs_curve.rmd', output_file='$PWD/srs_curve.pdf', output_format='pdf_document', clean=TRUE, knit_root_dir='$PWD')"
 Rscript -e "rmarkdown::render('srs_curve.rmd', output_file='$PWD/srs_curve.html', output_format='html_document', clean=TRUE, knit_root_dir='$PWD')"
